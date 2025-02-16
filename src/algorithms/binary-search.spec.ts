c383ee11fb16e15f74a7dc6902e2e09f15f42ea7
Danil Rodionov <Danil_Rodionov@epam.com>
2025-02-16 12:37:03 +0400

    feat: add binary search for shifted data

--- src/algorithms/binary-search.spec.ts
+++ src/algorithms/binary-search.spec.ts
@@ -1,28 +1,56 @@
 import { describe, it } from "node:test";
-import { binarySearch } from "./binary-search";
+import { binarySearch, binarySearchOnShiftedData } from "./binary-search";
 import assert from "node:assert";
 
 import * as TestHelpers from "../test.helpers";
 
 describe("Binary Search", () => {
-  const source: number[] = [];
-
-  for (let i = 0; i < 10000000; i++) {
-    source.push(i);
-  }
-
   function linearSearch(source: number[], target: number) {
     return source.findIndex((item) => item === target);
   }
 
   it("The Binary Search should work faster then the Linear Search", () => {
     const query = 5555555;
+    const source: number[] = [];
+
+    for (let i = 0; i < 10000000; i++) {
+      source.push(i);
+    }
 
     const { duration: binarySearchDuration, results: binarySearchResults } =
       TestHelpers.measureFnExecution(binarySearch, source, query);
 
     const { duration: linearSearchDuration, results: linearSearchResults } =
       TestHelpers.measureFnExecution(linearSearch, source, query);
+
+    console.info(`[Binary Search]: `, binarySearchDuration);
+    console.info(`[Linear Search]: `, linearSearchDuration);
+
+    assert.ok(binarySearchResults, linearSearchResults);
+    assert.ok(binarySearchDuration < linearSearchDuration);
+  });
+
+  it("The Binary Search on shifted data should work faster then the Linear Search on shifted data", () => {
+    const query = 99000;
+    const shiftedArr: number[] = [];
+
+    for (let i = 100000; i < 1000000; i++) {
+      shiftedArr.push(i);
+    }
+
+    for (let i = 0; i < 100000; i++) {
+      shiftedArr.push(i);
+    }
+
+    const { duration: binarySearchDuration, results: binarySearchResults } =
+      TestHelpers.measureFnExecution(
+        binarySearchOnShiftedData,
+        shiftedArr,
+        query
+      );
+
+    const { duration: linearSearchDuration, results: linearSearchResults } =
+      TestHelpers.measureFnExecution(linearSearch, shiftedArr, query);
 
     console.info(`[Binary Search]: `, binarySearchDuration);
     console.info(`[Linear Search]: `, linearSearchDuration);
--- src/algorithms/binary-search.ts
+++ src/algorithms/binary-search.ts
@@ -19,3 +19,29 @@ export function binarySearch(source: number[], target: number) {
 
   return -1;
 }
+
+export function binarySearchOnShiftedData(source: number[], target: number) {
+  let left = 0;
+  let right = source.length - 1;
+
+  while (left <= right) {
+    const midIndex = Math.floor((left + right) / 2);
+    if (source[midIndex] === target) {
+      return midIndex;
+    } else if (source[left] <= source[midIndex]) {
+      if (target >= source[left] && target < source[midIndex]) {
+        right = midIndex - 1;
+      } else {
+        left = midIndex + 1;
+      }
+    } else {
+      if (target > source[midIndex] && target <= source[right]) {
+        left = midIndex + 1;
+      } else {
+        right = midIndex - 1;
+      }
+    }
+  }
+
+  return -1;
+}
